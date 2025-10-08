3cfcdbb90283066b6ae237953982646ca868ffd7
sanzhik <tone23@mail.ru>
2025-10-08 23:03:46 +0500

    диплом2

--- db.php
+++ db.php
@@ -1,15 +1,16 @@
 <?php
-$host = getenv('DB_HOST') ?: "dpg-d3ia3cc9c44c73ajuq6g-a.render.com" ;
+
+$host = getenv('DB_HOST') ?: 'db';
 $user = "admin"; // логин из docker-compose.yml
 $pass = "GsZxkEXSahECU0kGEkeEqjGXDezH8FNn"; // пароль из docker-compose.yml
 $db   = "edukazdb";
 
 
 // Используем расширение pgsql для PostgreSQL
+
 $conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
 if (!$conn) {
     die("Ошибка подключения: " . pg_last_error());
 }
 ?>
-$host = getenv('DB_HOST') ?: 'db';
 
